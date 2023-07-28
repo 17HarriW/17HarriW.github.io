9cfae5a656a4e8ff5fb8a032f53243d525209aca
William Harris <17HarriW@fulford.york.sch.uk>
2023-07-28 17:12:28 +0100

    Added collapsible menu

--- scripts.js
+++ scripts.js
@@ -1,4 +1,18 @@
-﻿function darkMode() {
+﻿function ALevelsCollapse() {
+    var collapsibleImg = document.getElementById("ALevelCollapsibleImg");
+    var content = document.getElementById("ALevelContent");
+    content.classList.toggle("collapsed");
+
+    if (collapsibleImg.getAttribute("src") == "images/collapse.svg") {
+        collapsibleImg.src = "images/expand.svg";
+    }
+    else {
+        collapsibleImg.src = "images/collapse.svg";
+    }
+}
+
+
+function darkMode() {
     var darkModeImg = document.getElementById("darkModeImg");
     var element = document.body;
     element.classList.toggle("darkModeBody");
