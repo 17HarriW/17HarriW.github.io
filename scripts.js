ec017d723297f3a025f246ad9acf9c892e3ddf12
William Harris <17HarriW@fulford.york.sch.uk>
2023-07-28 15:05:28 +0100

    Added basic formats

---
+++ scripts.js
@@ -0,0 +1,12 @@
+﻿function darkMode() {
+    var darkModeImg = document.getElementById("darkModeImg");
+    var element = document.body;
+    element.classList.toggle("darkModeBody");
+
+    if (darkModeImg.getAttribute("src") == "images/moon.svg") {
+        darkModeImg.src = "images/sun.svg";
+    }
+    else {
+        darkModeImg.src = "images/moon.svg";
+    }
+}
